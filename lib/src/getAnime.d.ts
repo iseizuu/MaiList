d7c091ae06ea8d8e80bcbd4e33c179b4d63dbeb6
VeguiIzumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-08-24 01:02:01 +0700

    :y

--- lib/src/getAnime.d.ts
+++
@@ -1,8 +0,0 @@
-declare class Anime {
-    constructor();
-    /**
-     * @returns {Promise<string>} Anime Endpoint
-     */
-    anime(query: string): Promise<unknown>;
-}
-export { Anime };
--- lib/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './structures/Client';
-export * from './getAnime';
