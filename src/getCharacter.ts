4edcf335f037391983793633a0a81e7dcd108d40
VeguiIzumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-09-06 23:29:31 +0700

    Added Manga and Character search

---
+++ src/getCharacter.ts
@@ -0,0 +1,57 @@
+import { Client } from './structures/Client';
+const client = new Client()
+const req = require('node-superfetch');
+const q = `
+	query ($search: String) {
+		characters: Page (perPage: 1) {
+			results: characters (search: $search) { id }
+		}
+	}
+`;
+const s = `
+	query ($id: Int!) {
+		Character (id: $id) {
+			id
+			name {
+				first
+				last
+			}
+			image {
+				large
+				medium
+			}
+			description(asHtml: false)
+			siteUrl
+			media(page: 1, perPage: 5) {
+				edges {
+					node {
+						title {
+							english
+							userPreferred
+						}
+						type
+						siteUrl
+					}
+				}
+			}
+		}
+	}
+`;
+class Character {
+    public constructor() {
+    }
+    /**
+     * @returns {Promise<string>} Search Character
+     */
+    public character(query: string) {
+        return new Promise(async (resolve, reject) => {
+            const id = client.searchChar(query, q)
+            .then(async res => {
+                resolve(client.fetchChar(res, s));
+            })
+            .catch(reject)
+        });
+    };
+};
+
+export { Character };
---
+++ src/getManga.ts
@@ -0,0 +1,71 @@
+import { Client } from './structures/Client';
+const fetch = new Client()
+const searchManga = `
+	query ($search: String, $type: MediaType, $isAdult: Boolean) {
+		anime: Page (perPage: 10) {
+			results: media (type: $type, isAdult: $isAdult, search: $search) {
+				id
+				title {
+					english
+					romaji
+				}
+			}
+		}
+	}
+`;
+const resultManga = `
+	query media($id: Int, $type: MediaType) {
+		Media(id: $id, type: $type) {
+			id
+			idMal
+			title {
+				english
+				romaji
+			}
+			coverImage {
+				large
+				medium
+			}
+			startDate { year }
+			description(asHtml: false)
+			siteUrl
+			type
+			status
+			volumes
+			chapters
+			isAdult
+			meanScore
+		}
+	}
+`;
+
+class Manga {
+    public constructor() {
+    }
+    /**
+     * @returns {Promise<string>} Manga Endpoint
+     */
+    public manga(query: string) {
+        return new Promise((resolve, reject) => {
+            const id = fetch.search(query, 'MANGA', searchManga)
+            .then(res => {
+                resolve(fetch.fetch(res, 'MANGA', resultManga));
+            })
+            .catch(reject)
+        });
+    };
+
+    /**
+     * Getting The Mal Score || Maybe Usefull
+     */
+    public malScore(query : string,) {
+        return new Promise((resolve, reject) => {
+            const id = fetch.search(query, 'MANGA', searchManga)
+            .then(res => {
+                resolve(fetch.fetchMal(res));
+            })
+            .catch(reject)
+        });
+    }
+};
+export { Manga };
